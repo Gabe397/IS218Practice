6da9e8185d592f45a37ea45de0562e9c7c1a93ea
gabe3 <Dinya7991>
2019-02-27 19:06:49 -0500

    Task: Created the Test class for the File class.

---
+++ test/FileTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gabe3
+ * Date: 2/26/2019
+ * Time: 6:46 PM
+ */
+declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+class FileTest extends TestCase
+{
+    public function testFileInstantiate(): void
+    {
+        $file = new File();
+        $this-> assertInstanceOf(File::class,$file);
+    }
+
+    public function testCSVExists() :void
+    {
+        $this -> assertFileExists('data\data.csv');
+    }
+
+    public function testDirectoryExists():void
+    {
+        $this -> assertDirectoryExists('data');
+    }
+
+    public function testReadCSVIntoArray(): void
+    {
+        $this->assertTrue(
+            method_exists(File::class,'readCSVIntoArray')
+        );
+    }
+
+
+
+
+}
