02d759f43ec280d2c716a27eae85d79bff1c2d28
gabe3 <Dinya7991>
2019-02-27 19:06:21 -0500

    Task: Created a File class with the function of reading a csv into an array.

---
+++ src/File.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gabe3
+ * Date: 2/26/2019
+ * Time: 6:45 PM
+ */
+
+class File
+{
+    public static function readCSVIntoArray(string $fileName):array
+    {
+        $records = Array();
+        $count = 0;
+        $fieldName = '';
+
+        if(($handle = fopen("$fileName",'r')) !== FALSE)
+        {
+            while(($row = fgetcsv(handle,1000,",")) != FALSE)
+            {
+                if($count == 0)
+                {
+                    $fileName = $row;
+                }
+                else{
+                    $records[] = (object) array_combine($fieldName,$row);
+                }
+                $count++;
+
+            }
+            fclose($handle);
+        }
+        return $records;
+    }
+}
