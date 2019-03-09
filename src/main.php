0e1b1fda0ddc7bd9b8f28dd2a9fb8b9802384e0b
gabe3 <Dinya7991>
2019-03-09 14:55:14 -0500

    Task: Added a insert text function which will hold the $this -> html .= syntax

--- src/main.php
+++ src/main.php
@@ -6,10 +6,14 @@
  * Time: 3:15 AM
  */
 include('htmlTags.php');
+include('File.php');
 
 class main
 {
     private $html;
+    public function insert($text){
+        return ($this -> html .= $text );
+    }
 
     public function __construct()
     {
@@ -18,14 +22,22 @@ class main
 
         //$this->html .= htmlTags::tableStart( htmlTags::tableHead(htmlTags::tableRow(htmlTags::tHeaderColumn("Hello","yellow","bellow"))));
 
-        $myArray = array(1,2,3,4,5);
+        $myArray = array(1,2,3);
+        $var = File::readCSVIntoArray("../data/data.csv",'Car');
+        $keys = array_keys((array)$var[0]);
+
+
+        //$this->insert(htmlTags::tableStart());
+
 
         $this ->html .= htmlTags::tableStart();
         $this ->html .= htmlTags::tableHeadStart();
         $this ->html .= htmlTags::tableRowStart();
 
-        for ($x = 0; $x < count($myArray); $x++) {
-            $this ->html .= htmlTags::tHeaderColumn($myArray[$x]);
+        for ($x = 0; $x < count($var); $x++) {
+
+
+            $this ->html .= htmlTags::tHeaderColumn($keys[$x]);
         }
 
         $this ->html .= htmlTags::tableRowEnd();
@@ -33,10 +45,10 @@ class main
 
         $this ->html .= htmlTags::tableBodyStart();
 
-        for ($y = 0; $y < count($myArray); $y++) {
+        for ($y = 0; $y < count($var); $y++) {
             $this ->html .= htmlTags::tableRowStart();
-            for ($z = 0; $z < count($myArray) ; $z++) {
-                $this->html .= htmlTags::rowEntry($myArray[$z]);
+            for ($z = 0; $z < count($var) ; $z++) {
+                $this->html .= htmlTags::rowEntry($var[$z] -> $keys[0]);
             }
 
 
