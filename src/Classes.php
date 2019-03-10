7ffcbd038cae52483654459f401cd853647880a6
gabe3 <Dinya7991>
2019-03-10 04:19:15 -0400

    Task: Made a single classes.php which will hold all of the includes needed to run this program.

---
+++ src/Classes.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gabe3
+ * Date: 3/10/2019
+ * Time: 4:01 AM
+ */
+include "recFactory.php";
+include "ArrayFunctions.php";
+include "htmlTags.php";
+include "File.php";
+class Classes
+{
+
+}
