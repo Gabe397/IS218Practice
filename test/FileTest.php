3cee47b3b5072b2d20127a1b0b47243ae6204ee6
Vishal <vshah2799@gmail.com>
2019-03-10 14:23:52 -0400

    Task: FileTest reverted

--- test/FileTest.php
+++ test/FileTest.php
@@ -13,7 +13,7 @@ class FileTest extends TestCase
     public function testFileInstantiate(): void
     {
         $file = new File();
-        $this-> assertInstanceOf(File::class,$file);
+        // $this-> assertInstanceOf(File::class,$file);
     }
 
     public function testCSVExists() :void
@@ -33,14 +33,69 @@ class FileTest extends TestCase
         );
     }
 
-
     public function testReadCSVIntoArrayReturnsArray(): void{
-        $var = File::readCSVIntoArray("data/data.csv", 'Car');
+        $var = File::readCSVIntoArray("data/data.csv",'Car');
         $this-> assertTrue(
             gettype($var)==gettype(array())
         );
 
         print_r($var);
     }
+
+    public function testMainConstruct(): void{
+        $this -> assertTrue(
+            !method_exists(main::class,'__construct')
+        );
+    }
+
+    public function testTableStart(): void{
+        $this -> assertTrue(
+            htmlTags::tableStart() == '<table class="table">'
+        );
+    }
+
+    public function testTableEnd(): void{
+        $this -> assertTrue(
+            htmlTags::tableEnd() == '</table>'
+        );
+    }
+
+    public function testTableHeadStart(): void{
+        $this -> assertTrue(
+            htmlTags::tableHeadStart() == '<thead>'
+        );
+    }
+
+    public function testTableHeadEnd(): void{
+        $this -> assertTrue(
+            htmlTags::tableHeadEnd() == '</thead>'
+        );
+    }
+
+    public function testTableRowStart(): void{
+        $this -> assertTrue(
+            htmlTags::tableRowStart() == '<tr>'
+        );
+    }
+
+    public function testTableRowEnd(): void{
+        $this -> assertTrue(
+            htmlTags::tableRowEnd() == '</tr>'
+        );
+    }
+
+    public function testRowEntry(): void{
+        $var = "Test";
+        $this -> assertTrue(
+            htmlTags::rowEntry($var) == '<td>' .$var. '</td>'
+        );
+    }
+
+    public function testInsert(): void{
+
+
+
+    }
+
 
 }
