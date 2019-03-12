9125de140c25a9d5dca852eca784b91ae7a76edd
Vishal <vshah2799@gmail.com>
2019-03-12 16:19:56 -0400

    Task: Removed the file csvFunctionsTest.php and htmlTagsTest file. Organized the tests in FileTest.php

--- test/FileTest.php
+++ test/FileTest.php
@@ -11,46 +11,178 @@ use PHPUnit\Framework\TestCase;
 
 class FileTest extends TestCase
 {
-    public function testFileInstantiate(): void
-    {
-        $file = new File();
-        $this-> assertInstanceOf(File::class,$file);
-    }
 
-    public function testCSVExists() :void
-    {
-        $this -> assertFileExists('data\data.csv');
-    }
-
-    public function testDirectoryExists():void
-    {
-        $this -> assertDirectoryExists('data');
-    }
-
-    public function testReadCSVIntoArray(): void
-    {
-        $this->assertTrue(
-            method_exists(File::class,'readCSVIntoArray')
-        );
-    }
-
-    public function testReadCSVIntoArrayReturnsArray(): void{
-        $var = File::readCSVIntoArray("data/data.csv",'Car');
-        $this-> assertTrue(
-            gettype($var)==gettype(array())
-        );
-
-        print_r($var);
-    }
-
-    public function testMainConstruct(): void{
+    //These tests are for the htmlTags.php file and if they return the correct values
+    public function testTableStart(): void{
         $this -> assertTrue(
-          method_exists(main::class,'__construct')
-         );
+            htmlTags::tableStart() == '<table class="table">'
+        );
+    }
+
+    public function testTableEnd(): void{
+        $this -> assertTrue(
+            htmlTags::tableEnd() == '</table>'
+        );
+    }
+
+    public function testTableHeadStart(): void{
+        $this -> assertTrue(
+            htmlTags::tableHeadStart() == '<thead>'
+        );
+    }
+
+    public function testTableHeadEnd(): void{
+        $this -> assertTrue(
+            htmlTags::tableHeadEnd() == '</thead>'
+        );
+    }
+
+    public function testTableRowStart(): void{
+        $this -> assertTrue(
+            htmlTags::tableRowStart() == '<tr>'
+        );
+    }
+
+    public function testTableRowEnd(): void{
+        $this -> assertTrue(
+            htmlTags::tableRowEnd() == '</tr>'
+        );
+    }
+
+    public function testRowEntry(): void{
+        $var = "Test";
+        $this -> assertTrue(
+            htmlTags::rowEntry($var) == '<td>' .$var. '</td>'
+        );
+    }
+
+    public function testTableBodyEnd(): void{
+        $this->assertTrue(
+            htmlTags::tableBodyEnd() == '</tbody>'
+        );
+    }
+
+    public function testTHeaderColumn(): void{
+        $test = 'hello';
+        $this -> assertTrue(
+            htmlTags::tHeaderColumn($test) == '<th scope = "col">' .$test. '</th>'
+        );
+    }
+
+    public function testTHeaderRow(): void{
+        $test = 'hello';
+        $this -> assertTrue(
+            htmlTags::tHeaderRow($test) == '<th scope = "row">' .$test. '</th>'
+        );
+    }
+
+    public function testTableBodyStart(): void{
+        $this -> assertTrue(
+            htmlTags::tableBodyStart() == '<tbody>'
+        );
+    }
+
+
+
+
+    //These tests are for testing if the method exists
+    public function testPrintBeginOfTableExists():void{
+        $this->assertTrue(
+            method_exists(htmlTags::class, 'printBeginOfTable')
+        );
+    }
+
+    public function testPrintRowEndBodyStartForTableExists(): void{
+        $this->assertTrue(
+            method_exists(htmlTags::class, 'printRowEndBodyStartForTable')
+        );
+    }
+
+    public function testPrintTableBodyEndAndWholeTableEndExists(): void{
+        $this->assertTrue(
+            method_exists(htmlTags::class, 'printTableBodyEndAndWholeTableEnd')
+        );
+    }
+
+    public function testTableBodyEndExists(): void{
+        $this->assertTrue(
+            method_exists(htmlTags::class, 'tableBodyEnd')
+        );
+    }
+
+    public function testTableStartExists(): void{
+        $this -> assertTrue(
+            method_exists(htmlTags::class, 'tableStart')
+        );
+    }
+
+    public function testTableEndExists(): void{
+        $this -> assertTrue(
+            method_exists(htmlTags::class, 'tableEnd')
+        );
+
+    }
+
+    public function testTableHeadStartExists(): void{
+        $this -> assertTrue(
+            method_exists(htmlTags::class, 'tableHeadStart')
+        );
+
+    }
+
+    public function testTableHeadEndExists():void{
+        $this -> assertTrue(
+            method_exists(htmlTags::class, 'tableHeadEnd')
+        );
+
+    }
+
+    public function testTableRowStartExists():void{
+        $this -> assertTrue(
+            method_exists(htmlTags::class, 'tableRowStart')
+        );
+
+    }
+
+    public function testTableRowEndExists(): void{
+        $this -> assertTrue(
+            method_exists(htmlTags::class, 'tableRowEnd')
+        );
+
+    }
+
+    public function testTHeaderColumnExists(): void{
+        $this -> assertTrue(
+            method_exists(htmlTags::class, 'tHeaderColumn')
+        );
+
+    }
+
+    public function testTHeaderRowExists(): void{
+        $this -> assertTrue(
+            method_exists(htmlTags::class, 'tHeaderRow')
+        );
+
+    }
+
+    public function testRowEntryExists(): void{
+        $this -> assertTrue(
+            method_exists(htmlTags::class, 'rowEntry')
+        );
+
+    }
+
+    public function testTableBodyStartExists(): void{
+        $this -> assertTrue(
+            method_exists(htmlTags::class, 'tableBodyStart')
+        );
 
     }
 
 
+
+
+    //These are the arrayFunctions.php tests
     public function testCombineArray():void {
         $array1 = array(1,2,3);
         $array2 = array(4,5, 6);
@@ -60,15 +192,13 @@ class FileTest extends TestCase
             $combinedArray[1] == 4 && $combinedArray[2] == 5 && $combinedArray[3] == 6, "Arrays are Combined"
         );
     }
-
-    public function testArrayInstantiate() :void
+    public function testInstantiateArray() :void
     {
         $testArray = ArrayFunctions::instantiateArray();
         $this -> assertIsArray(
             $testArray
         );
     }
-
     public function testArrayKeys() :void{
         $testArray = array("key1" => 1, "key2" => 2);
         $keys = ArrayFunctions::arrayKeys($testArray);
@@ -78,13 +208,42 @@ class FileTest extends TestCase
             $keys[0] == "key1"
         );
     }
-
-    public function testCount():void{
+    public function testArrayCount():void{
         $testArray = array(1,2,3);
 
         $this->assertEquals(3,ArrayFunctions::arrayCount($testArray));
     }
 
+
+    //File.php tests
+    public function testReadCSVIntoArray(): void
+    {
+        $this->assertTrue(
+            method_exists(File::class,'readCSVIntoArray')
+        );
+    }
+    public function testReadCSVIntoArrayReturnsArray(): void{
+        $var = File::readCSVIntoArray("data/data.csv",'Car');
+        $this-> assertTrue(
+            gettype($var)==gettype(array())
+        );
+
+        print_r($var);
+    }
+
+
+
+    //main.php tests
+    public function testMainConstructExists(): void{
+        $this -> assertTrue(
+            method_exists(main::class,'__construct')
+        );
+
+    }
+    
+
+
+    //csvFunctions.php tests
     public function testCSVOpenExists():void
     {
         $this->assertTrue(
@@ -105,6 +264,32 @@ class FileTest extends TestCase
             method_exists(csvFunctions::class,'closeCSV')
         );
     }
+
+
+
+    //generic tests
+    public function testFileInstantiate(): void
+    {
+        $file = new File();
+        $this-> assertInstanceOf(File::class,$file);
+    }
+
+    public function testCSVExists() :void
+    {
+        $this -> assertFileExists('data\data.csv');
+    }
+
+    public function testDirectoryExists():void
+    {
+        $this -> assertDirectoryExists('data');
+    }
+
+
+
+
+
+
+
 
 
 
