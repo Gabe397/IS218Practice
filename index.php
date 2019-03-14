19c07de3c391d810ef40326a98ba7ab64b81faaa
Vishal <vshah2799@gmail.com>
2019-03-14 10:23:17 -0400

    Task: test

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
 
     <meta charset="utf-8">
-    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="css/main.css">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
@@ -24,7 +24,7 @@
             }
         }
     </style>
-    <link rel="stylesheet" href="../css/starter-template.css" >
+    <link rel="stylesheet" href="css/starter-template.css" >
 
     <title>Hello World</title>
 
