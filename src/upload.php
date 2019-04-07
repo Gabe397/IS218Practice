c27b64d20524bc4f5b9a153d09cff45d0c9f59e3
Martin Chukwu <mark7328chukwu@gmail.com>
2019-04-07 17:23:26 -0400

    Added upload file Edited index.php file with upload class and form to upload

---
+++ src/upload.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mark7
+ * Date: 4/7/2019
+ * Time: 2:40 PM
+ */
+$currentDir = getcwd();
+$uploadDirectory = "/uploads/";
+
+$errors = []; // Store all foreseen and unforseen errors here
+
+$fileExtensions = ['csv','xlsx']; // Get all the file extensions
+
+$fileName = $_FILES['fileToUpload']['name'];
+$fileSize = $_FILES['fileToUpload']['size'];
+$fileTmpName  = $_FILES['fileToUpload']['tmp_name'];
+$fileType = $_FILES['fileToUpload']['type'];
+$fileExtension = strtolower(end(explode('.',$fileName)));
+
+$uploadPath = $currentDir . $uploadDirectory . basename($fileName);
+
+if (isset($_POST['submit'])) {
+
+    if (! in_array($fileExtension,$fileExtensions)) {
+        $errors[] = "This file extension is not allowed. Please upload a csv or a xlsx";
+    }
+
+    if ($fileSize > 2000000) {
+        $errors[] = "This file is more than 2MB. Sorry, it has to be less than or equal to 2MB";
+    }
+
+    if (empty($errors)) {
+        $didUpload = move_uploaded_file($fileTmpName, $uploadPath);
+
+        if ($didUpload) {
+            echo "The file " . basename($fileName) . " has been uploaded";
+        } else {
+            echo "An error occurred somewhere. Try again or contact the admin";
+        }
+    } else {
+        foreach ($errors as $error) {
+            echo $error . "These are the errors" . "\n";
+        }
+    }
+}
+
+
+
