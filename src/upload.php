9318550a14f9e44f1bb69d13323c8b183f02aefa
gabe3 <Dinya7991>
2019-04-08 21:02:27 -0400

    Feature: Made some modifications to how the code is written, functionally the same.

--- src/upload.php
+++ src/upload.php
@@ -1,24 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mark7
- * Date: 4/7/2019
- * Time: 2:40 PM
- */
-$currentDir = getcwd();
-$uploadDirectory = "/uploads/";
+require __DIR__ . '/../vendor/autoload.php';
 
-$errors = []; // Store all foreseen and unforseen errors here
+
+$currentDir = directoryFunctions::getCurrentDirectory();
+$uploadDirectory = directoryFunctions::uploadsDirectory();
+
+$errors = arrayFunctions::instantiateArray(); // Store all foreseen and unforseen errors here
 
 $fileExtensions = ['csv','xlsx']; // Get all the file extensions
 
-$fileName = $_FILES['fileToUpload']['name'];
-$fileSize = $_FILES['fileToUpload']['size'];
-$fileTmpName  = $_FILES['fileToUpload']['tmp_name'];
-$fileType = $_FILES['fileToUpload']['type'];
-$fileExtension = strtolower(end(explode('.',$fileName)));
+$uploadFile = new upFile();
 
-$uploadPath = $currentDir . $uploadDirectory . basename($fileName);
+$fileExtension = strtolower(end(explode('.',$uploadFile->getFileName()))); //Needs an exception.
+
+$uploadPath = $currentDir . $uploadDirectory . basename($uploadFile->getFileName());
 
 if (isset($_POST['submit'])) {
 
@@ -26,15 +21,15 @@ if (isset($_POST['submit'])) {
         $errors[] = "This file extension is not allowed. Please upload a csv or a xlsx";
     }
 
-    if ($fileSize > 2000000) {
+    if ($uploadFile->getFileSize() > 2000000) {
         $errors[] = "This file is more than 2MB. Sorry, it has to be less than or equal to 2MB";
     }
 
     if (empty($errors)) {
-        $didUpload = move_uploaded_file($fileTmpName, $uploadPath);
+        $didUpload = move_uploaded_file($uploadFile->getFileTmpName(), $uploadPath);
 
         if ($didUpload) {
-            echo "The file " . basename($fileName) . " has been uploaded";
+            echo "The file " . basename($uploadFile->getFileName()) . " has been uploaded";
         } else {
             echo "An error occurred somewhere. Try again or contact the admin";
         }
