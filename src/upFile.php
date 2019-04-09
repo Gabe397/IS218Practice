c75b6bb0a601ade3bc5aa09c82a22b1f0db781f5
gabe3 <Dinya7991>
2019-04-08 21:01:43 -0400

    Task: Included a class for the file that we are uploading.

---
+++ src/upFile.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gabe3
+ * Date: 4/8/2019
+ * Time: 8:10 PM
+ */
+
+class upFile
+{
+
+    public function __construct()
+    {
+
+    }
+
+    public function getFileName()
+    {
+        $name = self::fileVar()['fileToUpload']['name'];
+        return $name;
+    }
+
+    public function getFileSize()
+    {
+        $size = self::fileVar()['fileToUpload']['size'];
+        return $size;
+    }
+
+    public function getFileTmpName(){
+        $tmpName = self::fileVar()['fileToUpload']['tmp_name'];
+        return $tmpName;
+    }
+
+    public function getFileType()
+    {
+        $fileType = self::fileVar()['fileToUpload']['type'];
+        return $fileType;
+    }
+
+    public static function fileVar()
+    {
+        return $_FILES;
+    }
+
+}
