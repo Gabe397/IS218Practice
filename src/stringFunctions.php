4e09e726d1f9e4e921648745eab32149682578b5
gabe3 <Dinya7991>
2019-04-09 20:17:33 -0400

    Feature: Included a string functions class that will hold all built in string functions

---
+++ src/stringFunctions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gabe3
+ * Date: 4/9/2019
+ * Time: 7:48 PM
+ */
+
+class stringFunctions
+{
+    public static function stringLower(string $word)
+    {
+        return strtolower($word);
+    }
+
+    public static function explodeString(string $boundStr,string $inputStr)
+    {
+        return explode($boundStr,$inputStr);
+    }
+}
