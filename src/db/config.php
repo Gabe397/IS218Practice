14857562258fb57703ed6552035cd6121401ce67
gabe3 <Dinya7991>
2019-04-18 11:29:59 -0400

    Task: Set up a DB and Made a Connection

---
+++ src/db/config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gabe3
+ * Date: 4/18/2019
+ * Time: 11:08 AM
+ */
+
+class config
+{
+    const PATH_TO_SQLITE_FILE = '../data/database.sqlite';
+
+}
--- src/index.php
+++ src/index.php
@@ -74,7 +74,16 @@
     <?php
         require __DIR__ . '/../vendor/autoload.php';
         new main();
+
+
+    $pdo = (new SQLiteConnection())->connect();
+    if ($pdo != null)
+        echo 'Connected to the SQLite database successfully!';
+    else
+        echo 'Whoops, could not connect to the SQLite database!';
     ?>
+
+    <br><br>
     <form action="upload.php" method="post" enctype="multipart/form-data">
         Select CSV to upload:
         <input type="file" name="fileToUpload" id="fileToUpload">
