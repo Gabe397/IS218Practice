afa1cda0269c02e3eacf334faf9ea8b78ce89c85
gabe3 <Dinya7991>
2019-04-25 03:04:55 -0400

    Task: Implemented Select Statement.

--- src/csvToDatabase.php
+++ src/csvToDatabase.php
@@ -12,18 +12,19 @@ class csvToDatabase
 
     public function __construct()
     {
-        $arrayObjects = csv::getRecords("../data/data.csv");//change so we put the file that the person uploads
+        //change so we put the file that the person uploads
+        $arrayObjects = csv::getRecords("../data/data.csv");
 
             //Creates the header must modify using $arrayObjects
-        $table = "TUTORIAL"; //change name maybe autoincrement
+        $table = "TUTORIAL7"; //change name maybe autoincrement
         try {
             $pdo = (new SQLiteConnection())->connect();
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//Error Handling
-            $sql = "CREATE table $table(
+            $stmt = "CREATE table $table(
             ID INTEGER PRIMARY KEY AUTOINCREMENT,
             first_name TEXT NOT NULL,
             last_name TEXT NOT NULL);";//Modify With Variables for Column
-            $pdo->exec($sql);
+            $pdo->exec($stmt);
 
 
             //Insertion Must modify for dynamic variables from $arrayObjects and multiple row insertion
@@ -31,13 +32,20 @@ class csvToDatabase
                 'name' => "try",
                 'surname' => "hello",
             ];
-            $sql = "INSERT INTO TUTORIAL (first_name, last_name) VALUES (:name, :surname)";
-            $sql = $pdo->prepare($sql);
-            $sql->execute($data);
+            $stmt = "INSERT INTO $table (first_name, last_name) VALUES (:name, :surname)";
+            $stmt = $pdo->prepare($stmt);
+            $stmt->execute($data);
 
 
+            //Selecting Data Needs word must be different function maybe? Also must be dynamic
+            $stmt = $pdo->prepare("SELECT * FROM $table");
+            $stmt->execute();
+            $user = $stmt->fetch();
 
-            print("Created $table Table.\n");
+            var_dump($user);
+
+
+            print("<br> <br> Created $table Table.\n");
 
         } catch (PDOException $e) {
             echo $e->getMessage();//Remove or change message in production code
