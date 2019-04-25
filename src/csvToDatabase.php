efdbee08761e41702e98e09fbc44916bd3bc91b6
gabe3 <Dinya7991>
2019-04-25 02:47:59 -0400

    Task: Implemented Code that will insert the values inside the Database Table

--- src/csvToDatabase.php
+++ src/csvToDatabase.php
@@ -10,41 +10,38 @@ require __DIR__ . '/../vendor/autoload.php';
 class csvToDatabase
 {
 
-    public function __construct(){
+    public function __construct()
+    {
         $arrayObjects = csv::getRecords("../data/data.csv");//change so we put the file that the person uploads
 
-        $table = "tcompany"; //change name maybe autoincrement
+            //Creates the header must modify using $arrayObjects
+        $table = "TUTORIAL"; //change name maybe autoincrement
         try {
-            $db = (new SQLiteConnection())->connect();
-            $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );//Error Handling
-            $sql ="CREATE table $table(
-            Prename VARCHAR( 50 ) NOT NULL, 
-            Name VARCHAR( 250 ) NOT NULL,
-            StreetA VARCHAR( 150 ) NOT NULL, 
-            StreetB VARCHAR( 150 ) NOT NULL, 
-            StreetC VARCHAR( 150 ) NOT NULL, 
-            County VARCHAR( 100 ) NOT NULL,
-            Postcode VARCHAR( 50 ) NOT NULL,
-            Country VARCHAR( 50 ) NOT NULL);" ;//Modify With Variables for Column
-            $db->exec($sql);
+            $pdo = (new SQLiteConnection())->connect();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//Error Handling
+            $sql = "CREATE table $table(
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            first_name TEXT NOT NULL,
+            last_name TEXT NOT NULL);";//Modify With Variables for Column
+            $pdo->exec($sql);
 
+
+            //Insertion Must modify for dynamic variables from $arrayObjects and multiple row insertion
+            $data = [
+                'name' => "try",
+                'surname' => "hello",
+            ];
+            $sql = "INSERT INTO TUTORIAL (first_name, last_name) VALUES (:name, :surname)";
+            $sql = $pdo->prepare($sql);
+            $sql->execute($data);
 
 
 
             print("Created $table Table.\n");
 
-        }
-        catch(PDOException $e) {
+        } catch (PDOException $e) {
             echo $e->getMessage();//Remove or change message in production code
         }
-
-
-
-
-
-
-
-
 
 
     }
